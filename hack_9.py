75f17116af37cc2786a25a1900887615c151c86e
jesus <jesus_lopez1997@hotmail.com>
2023-02-11 14:25:41 -0400

    hack 9 done

--- hack_9.py
+++ hack_9.py
@@ -2,8 +2,15 @@
 text: {"foo":"fookziman","bar":"barziman"} output => {"Foo":"Fooziman"}
 """
 
+import re
 
-def fn_hack_9():
-    result = {"foo":"fookziman","bar":"barziman"}
-    #...
-    return result
+def fn_hack_9(input):
+    input.popitem()
+
+    items = list(input.items())
+
+    new_key = items[0][0].capitalize()
+    new_value = items[0][1].capitalize()
+    new_value = re.sub('[k]+','', new_value)
+
+    return {new_key: new_value}
