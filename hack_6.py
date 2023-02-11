f95a1dc6445c6e5bb2110ff677233b7742d88dee
jesus <jesus_lopez1997@hotmail.com>
2023-02-11 11:59:14 -0400

    hack 6 done

--- hack_6.py
+++ hack_6.py
@@ -9,7 +9,15 @@ text: [] output => ["0"]
 """
 
 
-def fn_hack_6():
-    result = ["a","b","c","d","e"]
-    #...
+def fn_hack_6(input):
+    result = []
+    if input == []:
+        return ['0']
+
+    for i in range(len(input)):
+        if i % 2 == 0:
+            result.append(str(i + 1))
+        else:
+            result.append('-')
+    
     return result
