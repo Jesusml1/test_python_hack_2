ff4a86f4a52314046568c9a5a774d0413d94fb5a
jesus <jesus_lopez1997@hotmail.com>
2023-02-11 14:37:22 -0400

    hack 10 done

--- hack_10.py
+++ hack_10.py
@@ -3,7 +3,15 @@ text: [{"a":"b"},{"c","d"},{"e":"f"}] output => [{"1":"2"},{"3","4"},{"5":"6"}]
 """
 
 
-def fn_hack_10():
-    result = [{"a":"b"},{"c","d"},{"e":"f"}]
-    #...
+def fn_hack_10(input):
+
+    result = []
+    counter = 1
+    for i in range(len(input)):
+        if i % 2 == 0:
+            result.append({str(counter): str(counter + 1)})
+        else:
+            result.append({str(counter), str(counter + 1)})
+        counter += 2
+
     return result
