38c1a9b9c734c206fc94a5fb516d5bdef2479c2e
jesus <jesus_lopez1997@hotmail.com>
2023-02-11 00:19:17 -0400

    hack 4 done

--- hack_4.py
+++ hack_4.py
@@ -7,7 +7,12 @@ text: "qux" output => "qux"
 """
 
 
-def fn_hack_4():
-    result = "fooziman"
-    #...
+def fn_hack_4(input):
+
+    splited_word = list(input)
+    if len(splited_word) > 3:
+        splited_word.pop()
+        splited_word.pop(0)
+
+    result = ''.join(splited_word)
     return result
