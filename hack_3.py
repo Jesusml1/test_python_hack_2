4d71635d3bac578bfc0a0d5a466db2fb78869153
jesus <jesus_lopez1997@hotmail.com>
2023-02-10 22:33:21 -0400

    hack 3 done

--- hack_3.py
+++ hack_3.py
@@ -15,7 +15,27 @@ text: "qux" output => "QvX"
 """
 
 
-def fn_hack_3():
-    result = "fooziman"
-    #...
+def fn_hack_3(input):
+
+    splited_word = list(input)
+    new_word = []
+
+    for i in range(len(splited_word)):
+        if splited_word[i] == 'a':
+            new_word.append('@')
+        elif splited_word[i] == 'e':
+            new_word.append('3')
+        elif splited_word[i] == 'i':
+            new_word.append('¡')
+        elif splited_word[i] == 'o':
+            new_word.append('0')
+        elif splited_word[i] == 'u':
+            new_word.append('v')
+        elif i == 0 or i == len(splited_word) - 1:
+            new_word.append(splited_word[i].upper())
+        else:
+            new_word.append(splited_word[i])
+        
+    result = ''.join(new_word)
+
     return result
