313ee987259b34d1aa048646af01c6935035dd80
jesus <jesus_lopez1997@hotmail.com>
2023-02-10 21:07:30 -0400

    hack 2 done

--- hack_2.py
+++ hack_2.py
@@ -7,7 +7,17 @@ text: "qux" output => "qx"
 """
 
 
-def fn_hack_2():
-    result = "fooziman"
-    #...
+def fn_hack_2(input):
+
+    word = list(input)
+    word_without_vowels = []
+
+    for i in range(len(word)):
+        if word[i] != 'a' and word[i] != 'e' and word[i] != 'i' and word[i] != 'o' and word[i] != 'u':
+            word_without_vowels.append(word[i])
+
+    result = ''.join(word_without_vowels)
+
     return result
+
+# print(fn_hack_2("fooziman"))
